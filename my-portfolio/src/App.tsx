4046283aef7dd33c261652bf3aec111e9cdfbd74
herbertribeiro19 <hebert11223@gmail.com>
2023-11-23 20:13:53 -0300

    React e TypeScript

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 10)}>
+        <button onClick={() => setCount((count) => count + 30)}>
           count is {count}
         </button>
         <p>
