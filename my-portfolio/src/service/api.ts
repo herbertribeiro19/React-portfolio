e504c94ab92d4b9a0c4a7aae4bf45eae118144c1
herbertribeiro19 <hebert11223@gmail.com>
2023-11-23 21:21:22 -0300

    update

--- my-portfolio/src/service/api.ts
+++ my-portfolio/src/service/api.ts
@@ -1,20 +1,24 @@
 import axios, { AxiosResponse } from 'axios';
 
-export interface Repository{
+export interface Repository {
     id: number;
     name: string;
     description: string;
     html_url: string;
 }
 
-export interface User{
+export interface User {
     name: string;
     bio?: string;
     company?: string;
-    avatar_url:string;
+    avatar_url: string;
 }
 
 const GitHubService = {
+    getUser: aSync(username: string) Promise=User > => {
+    try {
+        const response: AxiosResponse<User> = await axios.get('https://api.github.com/users/${}')
+    }
+    }
 
-
-}
+}   
